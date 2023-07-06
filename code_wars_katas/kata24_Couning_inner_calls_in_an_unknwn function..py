d83f1bd58e7fdf4b92f31681550ed9077c892a7e
dkiexe <116086325+dkiexe@users.noreply.github.com>
2023-07-06 06:40:54 -1200

    Add files via upload

---
+++ code_wars_katas/kata24_Couning_inner_calls_in_an_unknwn function..py
@@ -0,0 +1,29 @@
+# The aim of this kata is to determine the number of sub-function calls made by an unknown function.
+
+# You have to write a function named count_calls which:
+
+# takes as parameter a function and its arguments (args, kwargs)
+
+# calls the function
+
+# returns a tuple containing:
+
+# the number of function calls made inside it and inside all the sub-called functions recursively
+
+# the function return value.
+
+# NB: The call to the function itself is not counted.
+
+# HINT: The sys module may come in handy.
+
+import sys
+
+def count_calls(func, *args, **kwargs):
+    """Count calls in function func"""
+    count_calls.calls_count = -1
+    def Tracer(frame, event, args):
+        if event == 'call':
+            count_calls.calls_count += 1
+    sys.settrace(Tracer)
+    rv = func(*args, **kwargs)
+    return count_calls.calls_count, rv
